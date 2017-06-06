8beecdfbb16a6b8460e9af049fb39e44ebff249b
Julien Rouhaud <julien.rouhaud@free.fr>
2017-06-06 18:27:32 +0200

    Update copyright year

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Open PostgreSQL Monitoring'
-copyright = u'2014-2016, OPMDG'
+copyright = u'2012-2017, OPMDG'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
